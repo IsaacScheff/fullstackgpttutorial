905660f0ee7e970228970e12a469157dd9409fd9
Isaac Scheff <isaac1996@comcast.net>
2024-03-07 21:15:44 -0500

    Created server endpoint

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,30 @@ app.use(cors());
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
+const openaiOrg = process.env.OPENAI_ORG;
+const openaiApiKey = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI({
+    organization: openaiOrg,
+    apiKey: openaiApiKey,
+});
+
+app.post("/", async (request, response) => {
+    const { chats } = request.body;
+
+    const result = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are a EbereGPT. You can help with graphic design tasks",
+          },
+          ...chats,
+        ],
+      });
+
+      response.json({
+        output: result.data.choices[0].message,
+      });
+});
